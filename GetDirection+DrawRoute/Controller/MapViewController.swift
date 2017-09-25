f37c08f1be13b88f85ddf2419f58c33dc6212e02
devxris <chuwei0915@gmail.com>
2017-09-25 17:02:13 +0800

    Simple UI and Setup

---
+++ GetDirection+DrawRoute/Controller/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  GetDirection+DrawRoute
+//
+//  Created by Chris Huang on 25/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+	@IBOutlet weak var mapView: MKMapView!
+	
+	
+}
